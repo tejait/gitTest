f2604a4b065b91b3813fa1786c8ae4abdd09a67c
balaj <balaj@BALAJI>
2024-04-20 12:41:46 +0530

    Balaji changes

--- src/com/tejait/b10/Balaji.java
+++ src/com/tejait/b10/Balaji.java
@@ -3,7 +3,8 @@ package com.tejait.b10;
 public class Balaji {
 
 	public static void main(String[] args) {
-	System.out.println("Balaji Bharatha");
+	System.out.println("Balaji Bharatha  ");
+	System.out.println("Balaji Bharatha 251 ");
 	}
 
 }

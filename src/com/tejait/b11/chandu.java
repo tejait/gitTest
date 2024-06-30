0391f2e363ea5c95f705a775bde61609ed5f6286
sekha <sekha@chandu>
2024-06-30 18:30:22 +0530

    new chaange

--- src/com/tejait/b11/chandu.java
+++ src/com/tejait/b11/chandu.java
@@ -1,5 +1,16 @@
 package com.tejait.b11;
 
 public class chandu {
-
+public static void main(String[] args) {
+	System.out.println("Chandu changes");
+	System.out.println("new changes");
+	System.out.println("one new changes");
 }
+}
+// right click on project
+// team 
+//commit
+//move your files un staged to staged (select your file click plus symbol)
+//names made changes
+// commit and push
+//your user name and password is teams token what is shared

77ae15a0c8c2a13c33bc58749132c7936d6c9196
chint <teja.chintapalli246@gmail.com>
2024-06-30 18:42:21 +0530

    teja commit

---
+++ src/com/tejait/b11/TejaB11.java
@@ -0,0 +1,14 @@
+package com.tejait.b11;
+
+public class TejaB11 {
+public static void main(String[] args) {
+	System.out.println("Teja Changes");
+}
+}
+// right click on project 
+// team
+// commit
+// move your files unstaged to staged  (select your file click plus symbol)
+// name mades changes
+//commit and push
+// your user name & pwd is teams token what ia shared

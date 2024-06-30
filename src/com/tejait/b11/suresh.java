016ec9a4ab85ca9b7976b19093264ba13c6d97f3
ADMIN <ADMIN@DESKTOP-GIUKI86>
2024-06-30 18:05:52 +0530

    commit changes suresh

---
+++ src/com/tejait/b11/suresh.java
@@ -0,0 +1,16 @@
+package com.tejait.b11;
+
+public class suresh {
+	public static void main(String[] args) {
+		System.out.println("suresh changes for b11");
+	}
+
+}
+
+
+// right click on project
+// team
+// commit
+// move your files unstaged to stage (select your file click plus symbol)
+// name mades changes
+// commit and push

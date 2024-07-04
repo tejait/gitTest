e6c68df0d8ad633d1bc66e550f8d7686ce515b4b
sekha <sekha@chandu>
2024-07-04 11:49:11 +0530

    its chandu commit

--- src/com/tejait/b11/chandu.java
+++ src/com/tejait/b11/chandu.java
@@ -4,13 +4,14 @@ public class chandu {
 public static void main(String[] args) {
 	System.out.println("Chandu changes");
 	System.out.println("new changes");
-	System.out.println("one new changes");
+	System.out.println("one more new changes");
+    System.out.println("project kalki");;
 }
 }
 // right click on project
 // team 
 //commit
-//move your files un staged to staged (select your file click plus symbol)
+//move your files unstaged to staged (select your file click plus symbol)
 //names made changes
 // commit and push
 //your user name and password is teams token what is shared
